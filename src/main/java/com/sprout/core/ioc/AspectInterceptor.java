d40aeb903c2460420dc8a4c7fab29ad268513409
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2026-07-25 18:54:09 +0300

    feat: add @MyTransactional and @MyRetry aspects to the method interceptor

--- src/main/java/com/sprout/core/ioc/AspectInterceptor.java
+++ src/main/java/com/sprout/core/ioc/AspectInterceptor.java
@@ -1,7 +1,10 @@
 package com.sprout.core.ioc;
 
 import com.sprout.core.annotation.Logged;
+import com.sprout.core.annotation.MyRetry;
+import com.sprout.core.annotation.MyTransactional;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -18,21 +21,93 @@ public class AspectInterceptor implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        boolean logged = target.getClass().getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Logged.class);
-
-        if (logged)
-            logger.info("--- [LOG] Starting method " + method.getName() + " ---");
-
-        Object response;
         try {
-            response = method.invoke(target, args);
+            if (method.getDeclaringClass() == Object.class) {
+                return switch (method.getName()) {
+                    case "equals" -> proxy == args[0];
+                    case "hashCode" -> System.identityHashCode(proxy);
+                    default -> method.invoke(target, args);
+                };
+            }
         } catch (InvocationTargetException e) {
             throw e.getTargetException();
         }
 
-        if (logged)
-            logger.info("--- [LOG] Finished method " + method.getName() + " ---");
+        Method targetMethod;
+        try {
+            targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException("Cannot find " + method.getName() + " on " + target.getClass(), e);
+        }
 
-        return response;
+        boolean logged = resolveAnnotation(method, targetMethod, Logged.class) != null;
+        boolean transactional = resolveAnnotation(method, targetMethod, MyTransactional.class) != null;
+        boolean isTransactionActive = false;
+        MyRetry retryable = resolveAnnotation(method, targetMethod, MyRetry.class);
+
+        if (logged)
+            logger.info("--- [LOG] Starting method " + method.getName() + " ---");
+
+        if (transactional) {
+            logger.info("--- BEGIN TX ---");
+            isTransactionActive = true;
+        }
+
+        try {
+            Object response = null;
+            if (retryable != null) {
+                int times = Math.max(1, retryable.times());
+                Throwable exception = null;
+
+                for (int i = 0; i < times; i++) {
+                    if (!isTransactionActive && transactional) {
+                        isTransactionActive = true;
+                        logger.info("--- BEGIN TX ---");
+                    }
+                    try {
+                        response = method.invoke(target, args);
+                        exception = null;
+                        break;
+                    } catch (InvocationTargetException e) {
+                        exception = e;
+                        if (transactional) {
+                            isTransactionActive = false;
+                            logger.info("--- ROLLBACK TX ---");
+                        }
+                        if (i < times - 1)
+                            logger.info("--- Retrying method... (Attempt " + (i + 1) + "/" + times + ") ---");
+                    }
+                }
+
+                if (exception != null)
+                    throw exception;
+            } else {
+                response = method.invoke(target, args);
+            }
+
+            if (transactional)
+                logger.info("--- COMMIT TX ---");
+
+            if (logged)
+                logger.info("--- [LOG] Finished method " + method.getName() + " ---");
+
+            return response;
+        } catch (Throwable e) {
+            if (logged)
+                logger.info("--- [LOG] Exception thrown by: " + method.getName() + " ---");
+            if (transactional && isTransactionActive)
+                logger.info("--- ROLLBACK TX ---");
+            throw e instanceof InvocationTargetException ? ((InvocationTargetException) e).getTargetException() : e;
+        }
+    }
+
+    private <T extends Annotation> T resolveAnnotation(Method method, Method targetMethod, Class<T> annotationClass) {
+        T annotation = targetMethod.getAnnotation(annotationClass);
+
+        annotation = annotation == null
+                ? method.getAnnotation(annotationClass)
+                : annotation;
+
+        return annotation;
     }
 }
