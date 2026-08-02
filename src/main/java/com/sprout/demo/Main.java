965b1a53124fca565aebf83966777acdc8f6ec39
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2026-08-02 22:44:05 +0300

    refactor: remove unused imports and simplify main method

--- src/main/java/com/sprout/demo/Main.java
+++ src/main/java/com/sprout/demo/Main.java
@@ -2,8 +2,6 @@ package com.sprout.demo;
 
 import com.sprout.core.ioc.BeanContainer;
 import com.sprout.demo.service.PaymentService;
-import com.sprout.demo.service.ReportService;
-import com.sprout.demo.service.DetailedAuditService;
 
 public class Main {
 
@@ -11,13 +9,7 @@ public class Main {
     public static void main(String[] args) {
         BeanContainer container = new BeanContainer();
         container.start("com.sprout.demo");
-
         PaymentService psv = container.getBean(PaymentService.class);
-
         System.out.println(psv.pay("1234", 50));
-        container.getBean(ReportService.class).generate();
-
-        DetailedAuditService auditService = container.getBean(DetailedAuditService.class);
-        auditService.audit();
     }
 }
